7869594787793359600d11ae2e6d7b6c5d82106f
Zisheng Ye <zisheng_ye@outlook.com>
2023-03-22 14:39:29 +0000

    update prerequisites to use nvcc_wrapper in kokkos

---
+++ prerequisites.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+sudo apt install clang-format
+
+# install cuda driver
+
+# install pytorch
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo apt-get update
+sudo apt-get install libcudnn8=8.5.0.*-1+cuda11.7
+sudo apt-get install libcudnn8-dev=8.5.0.*-1+cuda11.7
+
+pip install torch torchvision torchaudio
+
+# install kokkos
+cd ~
+mkdir package
+cd package
+git clone https://github.com/kokkos/kokkos.git
+cd kokkos
+export OMPI_CXX=~/package/kokkos/bin/nvcc_wrapper
+mkdir build
+cd build
+export CXX=~/package/kokkos/bin/nvcc_wrapper
+cmake -D CMAKE_INSTALL_PREFIX:PATH=~/kokkos/ -D Kokkos_ENABLE_OPENMP:BOOL=ON -D Kokkos_ENABLE_SERIAL:BOOL=OFF -D Kokkos_ENABLE_CUDA:BOOL=ON -D Kokkos_ENABLE_CUDA_LAMBDA:BOOL=ON -D Kokkos_ARCH_AMPERE86:BOOL=ON -D CMAKE_CXX_STANDARD=17 ../
+make install -j
+
+# prepare hignn directory
+cd ~
+cd hignn
+
+git clone https://github.com/pybind/pybind11.git
+git clone https://gitlab.com/libeigen/eigen.git
+
+git config --global user.email "zisheng_ye@outlook.com"
+git config --global user.name "Zisheng Ye"
