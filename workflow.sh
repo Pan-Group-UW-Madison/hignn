9a88c4ea8d0b0a0f72ec10457d6ab561e64734d3
Zisheng Ye <zisheng_ye@outlook.com>
2023-07-23 18:42:29 -0500

    update workflow

--- workflow.sh
+++ workflow.sh
@@ -1,21 +1,38 @@
+#!/usr/bin/bash
+
+if [ $1 == "clean" ]; then
+    make clean
+
+    rm CMakeCache.txt
+    rm -rf CMakeFiles/
+    
+    exit
+fi
+
+export PATH=$PATH:/opt/openmpi/bin
+
+Kokkos_PATH=/opt/kokkos
+LibTorch_PATH=/opt/libtorch
+
 if [ $1 == "rebuild" ]; then
     make clean
 
     rm CMakeCache.txt
     rm -rf CMakeFiles/
 
-    export CXX=~/kokkos/bin/nvcc_wrapper
-    cmake -DCMAKE_PREFIX_PATH="`python3 -c 'import torch;print(torch.utils.cmake_prefix_path)'`;~/kokkos/" -DCMAKE_CXX_EXTENSIONS=Off ./
+    export CXX=$Kokkos_PATH/bin/nvcc_wrapper
+    cmake -DCMAKE_PREFIX_PATH="$LibTorch_PATH/share/cmake/;$Kokkos_PATH" -DCMAKE_CXX_EXTENSIONS=Off ./
 fi
 
 cmake_result=$?
 if [ $cmake_result -ne 0 ]; then
     echo -e "${RED}cmake fails${NC}"
+    exit
 else
     echo -e "${Green}cmake suceeds${NC}"
 fi
 
-export OMPI_CXX=~/kokkos/bin/nvcc_wrapper
+export OMPI_CXX=$Kokkos_PATH/bin/nvcc_wrapper
 export NVCC_WRAPPER_DEFAULT_COMPILER=/usr/bin/g++
 export CUDA_LAUNCH_BLOCKING=1
 
@@ -29,9 +46,8 @@ else
     echo "make suceeds"
 fi
 
-export OMP_NUM_THREADS=30
-export OMP_PLACES=cores
-export OMP_PROC_BIND=close
-export CUDA_VISIBLE_DEVICES=0
+export OMP_NUM_THREADS=10
+export OMP_PLACES=sockets
+export OMP_PROC_BIND=spread
 
-./hignn
+mpirun -n 4 --allow-run-as-root ./hignn > output.txt 2>&1
